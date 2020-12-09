5aac220e4c7e7c7b649f0a0cb40b400a0ef27756
600798 <600798@600798PC>
2020-12-09 19:12:45 +0500

    Добавлен тип данных StrPlayer

--- TestPluginFixes/src/Plugin/StrPlayer.java
+++ TestPluginFixes/src/Plugin/StrPlayer.java
@@ -1,7 +1,27 @@
 package Plugin;
 
+import java.io.File;
+import java.io.IOException;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+/**
+ * @author 600798
+ *
+ */
 public class StrPlayer {
+	
+	private Main plugin;
+	public StrPlayer(Main plugin) {
+		this.plugin = plugin;
+	}
+	
+	public StrPlayer(String ip, boolean boolean1, long long1, String string2, boolean boolean2, long long2,
+			String string3, boolean boolean3, String string4, boolean boolean4, boolean boolean5, boolean boolean6,
+			long long3) {
+		// TODO Auto-generated constructor stub
+	}
 	public String getNickname() {
 		return nickname;
 	}
@@ -107,9 +127,53 @@ public class StrPlayer {
 	private long playTime = 0;
 	
 	// --------------------------------------------------------
+	public void setPlayerCfg(StrPlayer p) {
+		File cfg = new File(plugin.getDataFolder() + File.separator + "StrPlayers.yml");
+		FileConfiguration f = YamlConfiguration.loadConfiguration(cfg);
+		//f.set(p.getNickname()+"."+p.getIp() );
+		//h.set("locations." + name + ".x", loc.getBlockX());
+		f.set(nickname+".ip",ip);
+		f.set(nickname+".banned",banned);
+		f.set(nickname+".banTime",banTime);
+		f.set(nickname+".banReason",banReason);
+		f.set(nickname+".muted",muted);
+		f.set(nickname+".muteTime",muteTime);
+		f.set(nickname+".muteReason",muteReason);
+		f.set(nickname+".flying",flying);
+		f.set(nickname+".flyReason",flyReason);
+		f.set(nickname+".invurable",invurable);
+		f.set(nickname+".vanished",vanished);
+		f.set(nickname+".saturated",saturated);
+		f.set(nickname+".playTime",playTime);
+		
+		try {
+			f.save(cfg);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+	}
 	
-	
-	
+	public StrPlayer getPlayerCfg(String name) {
+		File cfg = new File(plugin.getDataFolder() + File.separator + "StrPlayers.yml");
+		FileConfiguration f = YamlConfiguration.loadConfiguration(cfg);
+		//h.getDouble("locations." + name + ".x"),
+		StrPlayer p = new StrPlayer(
+				f.getString(name+".ip"),
+				f.getBoolean(nickname+".banned"),
+				f.getLong(nickname+".banTime"),
+				f.getString(nickname+".banReason"),
+				f.getBoolean(nickname+".muted"),
+				f.getLong(nickname+".muteTime"),
+				f.getString(nickname+".muteReason"),
+				f.getBoolean(nickname+".flying"),
+				f.getString(nickname+".flyReason"),
+				f.getBoolean(nickname+".invurable"),
+				f.getBoolean(nickname+".vanished"),
+				f.getBoolean(nickname+".saturated"),
+				f.getLong(nickname+".playTime"));
+		return p;
+	}
 	
 	
 	
