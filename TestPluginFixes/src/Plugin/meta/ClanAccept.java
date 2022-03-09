d43eb56248089f43386ec9e27024b7ecebe4017c
Vladimir Kukavskiy <kukavskiy@mail.ru>
2022-03-10 03:28:46 +0500

    fix clanaccept

--- TestPluginFixes/src/Plugin/meta/ClanAccept.java
+++ TestPluginFixes/src/Plugin/meta/ClanAccept.java
@@ -28,30 +28,41 @@ public class ClanAccept implements CommandExecutor{
 		return true;
 	}
 	private void joinClan(Player p) {
-		String clanName = getClanName(p);
-		if((clanName ==null)||(clanName.equals("0"))) {
+		String inviteName = getInviteName(p);
+		String clanName = getClanName(p.getName());
+		if((inviteName ==null)||(inviteName.equals("0"))) {
 			MessageManager.getManager().msg(p, MessageType.BAD, "У вас нет активных приглашений");
+			return;
+		}
+		if(!(clanName ==null)||(inviteName.equals("0"))) {
+			MessageManager.getManager().msg(p, MessageType.BAD, "Сначала покиньте ваш клан!");
 			return;
 		}
 		File clans = new File(plugin.getDataFolder() + File.separator + "Clans.yml");
 		FileConfiguration c = YamlConfiguration.loadConfiguration(clans);
-		List<String> listmembers = c.getStringList("Clans."+clanName+".members");
-		c.set("Players."+p.getName()+".clan", clanName);
+		List<String> listmembers = c.getStringList("Clans."+inviteName+".members");
+		c.set("Players."+p.getName()+".clan", inviteName);
 		c.set("Players."+p.getName()+".invite", "0");
 		listmembers.add(p.getName());
-		c.set("Clans."+clanName+".members", listmembers);
+		c.set("Clans."+inviteName+".members", listmembers);
 		try {
 			c.save(clans);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "lp user "+p.getName()+" parent add clan."+clanName);
-		MessageManager.getManager().msg(p, MessageType.GOOD, "Вы вступили в клан §6"+clanName+"§a перезайдите в игру для обновления в TAB");
+		plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "lp user "+p.getName()+" parent add clan."+inviteName);
+		MessageManager.getManager().msg(p, MessageType.GOOD, "Вы вступили в клан §6"+inviteName+"§a перезайдите в игру для обновления в TAB");
 	}
-	private String getClanName(Player p) {
+	private String getInviteName(Player p) {
 		File clans = new File(plugin.getDataFolder() + File.separator + "Clans.yml");
 		FileConfiguration c = YamlConfiguration.loadConfiguration(clans);
 		String output = c.getString("Players."+p.getName()+".invite");
+		return output;
+	}
+	private String getClanName(String p) {
+		File clans = new File(plugin.getDataFolder() + File.separator + "Clans.yml");
+		FileConfiguration c = YamlConfiguration.loadConfiguration(clans);
+		String output = c.getString("Players."+p+".clan");
 		return output;
 	}
 }
